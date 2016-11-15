6b081ed6af3d800361781b17564a3e4dde1dd76a
David R. Forester <d.raymond.forester@gmail.com>
2016-11-15 15:31:22 -0600

    Update video_frames_classifier.py

--- video_frames_classifier.py
+++ video_frames_classifier.py
@@ -2,7 +2,16 @@
 """
 Modified on Monday November 14 09:45:13 2016
 
-VFC is an example of using a pretrained neural network to check frames from video for presence of specific classes of objects. It was designed to be used on multiple camera-enabled systems operating on a common network. A neural network is loaded into memory (in this case the resnet_50_weights for theano as in this Keras example: https://github.com/fchollet/deep-learning-models/blob/master/resnet50.py), then threads are spawned to listen for UDP multicast messages on one or more ports as defined in "hostnames_ports_listen.csv" (this system's hostname must preceed one or more valid port numbers). Valid messages may contain two fields; the first is the command. If the command is "predict" then the second field is taken to be the file path of the video to analyze. The video frames are then input sequentially to the neural network which outputs probabilities for each of the 1000 classes in ImageNet. These class probabilities are used determine the likelihood that a vehicle (as defined in "vehicle_words.csv") is present in each image. A vehicle (car, truck, motorcycle, etc.) is considered present if a class in "vehicle_words.csv" is present in the top 20 predicted classes and has a probability > 0.09.
+VFC is an example of using a pretrained neural network to check frames from video for presence of specific classes of objects.
+It was designed to be used on multiple camera-enabled systems operating on a common network. A neural network is loaded into
+memory (in this case the resnet_50_weights for theano as in this Keras example:
+https://github.com/fchollet/deep-learning-models/blob/master/resnet50.py), then threads are spawned to listen for UDP multicast
+messages on one or more ports as defined in "hostnames_ports_listen.csv" (this system's hostname must preceed one or more valid
+port numbers). Valid messages may contain two fields; the first is the command. If the command is "predict" then the second
+field is taken to be the file path of the video to analyze. The video frames are then input sequentially to the neural network
+which outputs probabilities for each of the 1000 classes in ImageNet. These class probabilities are used determine the
+likelihood that a vehicle (as defined in "vehicle_words.csv") is present in each image. A vehicle (car, truck, motorcycle, etc.)
+is considered present if a class in "vehicle_words.csv" is present in the top 20 predicted classes and has a probability > 0.09.
 
 The code is run from the command-line like:
   $ python video_frames_classifier.py '224.1.1.1'
